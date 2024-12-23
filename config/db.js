81cc76d8db708dff95466af6af22416c1d0c1d84
Khushi singh <khushiss2004@gmail.com>
2024-12-23 20:26:34 +0530

    second commit

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import mongoose, { connect } from "mongoose";
-
+import colors from "colors";
 const connetDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Connected to MongoDb Database ${conn.connection.host}`);
+    console.log(
+      `Connected to MongoDb Database ${conn.connection.host}`.bgMagenta.white
+    );
   } catch (error) {
     console.log(`Error in MONGODB ${error}`.bgRed.white);
   }
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-<<<<<<< HEAD
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRouters from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRouters);
 
 //rest api
 app.get("/", (req, res) => {
@@ -23,35 +34,3 @@ app.listen(PORT, () => {
     `server running on ${process.env.DEV_MODE} mode on port${PORT}`.bgGreen.red
   );
 });
-//acSd2JBDcBcQTX6G--password username--> khushiss2004
-//add new user database--> password--> khushi@21S  username--> khushi21
-//223.237.21.170/32--> previous ip address
-=======
-import express from "express";
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-
-//configure env
-dotenv.config();
-
-//rest object
-const app = express();
-
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to mern stack project</h1>");
-});
-//PORT
-const PORT = process.env.PORT || 8080;
-
-//run
-app.listen(PORT, () => {
-  console.log(
-    `server running on ${process.env.DEV_MODE} mode on port${PORT}`.bgGreen.red
-  );
-});
-//acSd2JBDcBcQTX6G--password username--> khushiss2004
-//add new user database--> password--> khushi@21S  username--> khushi21
-//223.237.21.170/32--> previous ip address
->>>>>>> e89003d (first commit)
